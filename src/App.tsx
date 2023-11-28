9f6a138656e852f43db877a779e6be6a5070e753
Yu Hang Lee <lyu8572@gmail.com>
2023-11-28 20:51:28 +0800

    Add New Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import {
   AppShell,
   Burger,
   Group,
-  Skeleton,
   Button,
+  SegmentedControl,
+  Text,
+  Box,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
@@ -15,10 +17,20 @@ interface dataTypes {
   author: string;
 }
 
+type InnerObjectType = {
+  text: string;
+  author: string;
+};
+
+type MotivationTypes = {
+  [key: number]: InnerObjectType;
+};
 function App() {
   const [opened, { toggle }] = useDisclosure();
   const [data, setData] = React.useState<dataTypes>();
+  const [motivationData, setMotivationData] = React.useState<MotivationTypes>();
 
+  const [value, setValue] = React.useState('quote');
   async function updateQuote() {
     try {
       const response = await fetch(
@@ -34,14 +46,32 @@ function App() {
       // setData({ content: 'Opps... Something went wrong' });
     }
   }
+  // console.log(data, ' data');
 
+  async function updateMotivation() {
+    try {
+      const response = await fetch('https://type.fit/api/quotes');
+      const { statusCode, statusMessage, ...data } = await response.json();
+      if (!response.ok) throw new Error(`${statusCode} ${statusMessage}`);
+      setMotivationData(data);
+    } catch (error) {
+      // If the API request failed, log the error to console and update state
+      // so that the error will be reflected in the UI.
+      console.error(error);
+      // setData({ content: 'Opps... Something went wrong' });
+    }
+  }
   // Run `updateQuote` once when component mounts
   React.useEffect(() => {
     updateQuote();
+    updateMotivation();
   }, []);
 
   // Do not render until the first quote is loaded
   if (!data) return null;
+  if (!motivationData) return null;
+  console.log(motivationData, ' motivation');
+
   const date = new Date().toJSON().slice(0, 10);
   return (
     <MantineProvider>
@@ -65,30 +95,56 @@ function App() {
           </Group>
         </AppShell.Header>
         <AppShell.Navbar p="md">
-          Navbar
-          {Array(15)
-            .fill(0)
-            .map((_, index) => (
-              <Skeleton key={index} h={28} mt="sm" animate={false} />
-            ))}
+          <Text style={{ marginBottom: '16px' }}>Navbar</Text>
+          <SegmentedControl
+            fullWidth
+            size="lg"
+            orientation="vertical"
+            value={value}
+            onChange={setValue}
+            data={[
+              { label: 'Quote', value: 'quote' },
+              { label: 'Motivation', value: 'ng' },
+              // { label: 'Vue', value: 'vue' },
+              // { label: 'Svelte', value: 'svelte' },
+            ]}
+          />
         </AppShell.Navbar>
         <AppShell.Main>
-          {' '}
-          <div
-            style={{
-              padding: '10px',
-              fontFamily: 'Inter, san-serif',
-            }}
-          >
-            <h1>Quote Generator</h1>
-            <h2>By Yu Hang Lee</h2>
-            <p>{data.content}</p>
-            {data.author && <p>{data.author}</p>}
-            <Button variant="default" onClick={updateQuote}>
-              New Quote
-            </Button>
-            <p>Current date: {date}</p>
-          </div>
+          {value === 'quote' && (
+            <div
+              style={{
+                padding: '10px',
+                fontFamily: 'Inter, san-serif',
+              }}
+            >
+              <h1>Quote Generator</h1>
+              <p>{data.content}</p>
+              {data.author && <p>{data.author}</p>}
+              <Button variant="default" onClick={updateQuote}>
+                New Quote
+              </Button>
+              <p>Current date: {date}</p>
+            </div>
+          )}
+          {value === 'ng' && (
+            <div
+              style={{
+                padding: '10px',
+                fontFamily: 'Inter, san-serif',
+              }}
+            >
+              <Text>Motivation Generator</Text>
+              {Object.entries(motivationData).map(([key, data]) => (
+                <Box style={{ margin: '16px 0' }}>
+                  <Text size="md" key={key} style={{ fontWeight: 'bold' }}>
+                    {data.text}
+                  </Text>
+                  <Text size="sm">Author: {data.author}</Text>
+                </Box>
+              ))}
+            </div>
+          )}
         </AppShell.Main>
       </AppShell>
     </MantineProvider>
